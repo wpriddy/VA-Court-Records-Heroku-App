6dca6e2e3445871e02677829473267a608a644f1
Wyatt Priddy <wyattpriddy502gmail.com>
2022-02-13 18:47:11 -0500

    add data table to complete choropleth analysis

--- apps/choropleth.py
+++ apps/choropleth.py
@@ -1,12 +1,12 @@
 #%%
-from dash import dcc, html
+from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output
 from itertools import product
 import sys
 sys.path.insert(0, r'..')
 from app import app
-import json
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import pathlib
 from data.get_data import *
@@ -32,89 +32,103 @@ layout = html.Div(children = [
             }),
 
     html.Div([
-            dcc.Dropdown(
-                id='district_or_circuit',
-                options = [{'label': 'Circuit', 'value': 'circuit'}, {'label': 'District', 'value': 'district'}],
-                value = 'circuit',
-                searchable=False, 
-                style={'background-color': '#DDD7D7','width': '275px'},
-                className = 'row'
-            ),
+            html.Div(
+                dcc.Dropdown(
+                    id='district_or_circuit',
+                    options = [{'label': 'Circuit', 'value': 'circuit'}, {'label': 'District', 'value': 'district'}],
+                    value = 'circuit',
+                    searchable=False, 
+                    style={'background-color': '#DDD7D7','width': '100%'},
+                    className = 'row',
+                    placeholder = 'Select a Court'
+                ), style={'width': '16.2%', 'display': 'inline-block'}),
 
             html.Div(' a'*2,  style={'color':'white'}),
 
-            dcc.Dropdown(
-                id='time-series',
-                options = [{'label': str(k), 'value': k} for k in sorted(full_data['circuit'], reverse=True)],
-                value = [max(full_data['circuit'])],
-                searchable=False,
-                multi=True,
-                style={'background-color': '#DDD7D7','width': '275px'},
-                className = 'row'
-                ),
+            html.Div(
+                dcc.Dropdown(
+                    id='time-series',
+                    options = [{'label': str(k), 'value': k} for k in sorted(full_data['circuit'], reverse=True)],
+                    value = [max(full_data['circuit'])],
+                    searchable=False,
+                    multi=True,
+                    style={'background-color': '#DDD7D7','width': '100%'},
+                    className = 'row',
+                    placeholder = 'All Years'
+                ), style = {'width': '16.2%', 'display': 'inline-block'}),
 
             html.Div(' a'*2,  style={'color':'white'}),
 
-            dcc.Dropdown(
-                id='race',
-                options=[{'label': val, 'value': key} for key, val in sorted(race_map.items(), key=lambda item: item[1])],
-                value= [5],
-                searchable=False,
-                multi=True,
-                style={'background-color': '#DDD7D7','width': '275px'},
-                className = 'row'
-            ),
+            html.Div(
+                dcc.Dropdown(
+                    id='race',
+                    options=[{'label': val, 'value': key} for key, val in sorted(race_map.items(), key=lambda item: item[1])],
+                    value= [5],
+                    searchable=False,
+                    multi=True,
+                    style={'background-color': '#DDD7D7','width': '100%'},
+                    className = 'row',
+                    placeholder = 'All Races'
+                ), style = {'width': '16.2%', 'display': 'inline-block'}),
             
             html.Div(' a'*2,  style={'color':'white'}),
 
-            dcc.Dropdown(
-                id='gender',
-                options = [{'label': val, 'value': key} for key, val in sorted(sex_map.items(), key=lambda item: item[1])],
-                value = [1],
-                searchable=False,
-                multi=True, 
-                style={'background-color': '#DDD7D7','width': '275px'},
-                className = 'row'
-                ),
-            ], style={'display':'flex', 'margin':'1.5em'}
-        ),
+            html.Div(
+                dcc.Dropdown(
+                    id='gender',
+                    options = [{'label': val, 'value': key} for key, val in sorted(sex_map.items(), key=lambda item: item[1])],
+                    value = [1],
+                    searchable=False,
+                    multi=True, 
+                    style={'background-color': '#DDD7D7','width': '100%'},
+                    className = 'row', 
+                    placeholder = 'All Sexes'
+                ), style = {'width': '16.2%', 'display': 'inline-block'})
 
-        html.Div([
+        ], style={'display':'flex', 'margin':'1.5em'}
+    ),
 
+    html.Div([
+        html.Div(
             dcc.Dropdown(
                 id='adjust_per_capita',
                 options = [{'label': 'Per Capita Arrests', 'value': 'True'}, {'label': 'Absolute Arrests', 'value': 'False'}],
                 value = 'True',
                 searchable=False,
-                style={'background-color': '#DDD7D7','width': '375px'},
-                className = 'row'
-            ),
+                style={'background-color': '#DDD7D7','width': '100%'},
+                className = 'row',
+                placeholder = 'Select a Metric'
+            ), style =  {'width': '22%', 'display': 'inline-block'}),
 
-            html.Div(' a'*2,  style={'color':'white'}),
-
+        html.Div(' a'*2,  style={'color':'white'}),
+        
+        html.Div(
             dcc.Dropdown(
                 id='charge_type',
                 options = [{'label': val, 'value': key} for key, val in sorted(charge_map['circuit'].items(), key=lambda item: item[1])],
                 value = [0],
                 searchable=False,
                 multi=True, 
-                style={'background-color': '#DDD7D7','width': '375px'},
-                className = 'row'
-                ),
+                style={'background-color': '#DDD7D7','width': '100%'},
+                className = 'row',
+                placeholder = 'All Charges'
+            ), style =  {'width': '22%', 'display': 'inline-block'}),
             
-            html.Div(' a'*2,  style={'color':'white'}),
+        html.Div(' a'*2,  style={'color':'white'}),
 
+        html.Div(
             dcc.Dropdown(
                 id='disposition_type',
                 options = [{'label': val, 'value': key} for key, val in sorted(dispo_map['circuit'].items(), key=lambda item: item[1])],
                 value = [0],
                 searchable=False,
                 multi=True,
-                style={'background-color': '#DDD7D7','width': '375px'},
-                className = 'row'
-                ),
+                style={'background-color': '#DDD7D7','width': '100%'},
+                className = 'row',
+                placeholder = 'All Dispositions'
+            ), style =  {'width': '22%', 'display': 'inline-block'}),
             
-            html.H3('Summary Statistics', style={'margin-left': '140px'}, className='row')
+            html.H3('Data Table', style={'margin': 'auto'}, className='row')
             ], style={'display':'flex', 'margin':'1.5em'}
         ),
 
@@ -123,21 +137,48 @@ layout = html.Div(children = [
             dcc.Graph(
                 id='interactive-graphic',
                 style = {'border': '.5px solid black', 'margin-left': '28px'},
-                className = 'w3 row w3-twothird'),
+                className = 'w3-row w3-twothird'),
 
                 html.Div([
-                    html.Img(src = app.get_asset_url('temp_graph.png'), style={'height': '220px', 'margin-left':'150px', 'margin-right': '28px'}), 
                     
-                    html.Img(src = app.get_asset_url('temp_pie.jpg'), style={'height':'220px', 'margin-left':'150px', 'margin-right': '28px'})
-                    
-                    ], className = 'w3-row w3-rest')
-                ]),
-    ]
+                    dash_table.DataTable(
+                                    id = 'dataTable',
+                                    style_table = {'overflowY': 'scroll',
+                                                    'overflowX': 'scroll', 
+                                                    'margin-left': '4%', 
+                                                    'width': '117%', 
+                                                    'maxHeight': '453px',
+                                                    'border': '1px solid black'},
+                                    style_data = {'height': 'auto'},
+                                    style_data_conditional=[
+                                        {
+                                            'if': {'row_index': 'odd'},
+                                            'backgroundColor': 'rgb(220, 220, 220)',
+                                        }
+                                    ],
+                                    style_header={
+                                        'backgroundColor': 'rgb(210, 210, 210)',
+                                        'color': 'black',
+                                        'fontWeight': 'bold'
+                                    },
+                                    style_cell_conditional=[
+                                        {
+                                            'if': {'column_id': c},
+                                            'textAlign': 'left'
+                                        } for c in ['Area']
+                                    ],
+                                    sort_action = 'native'
+                                )
+                        ], className = 'w3-row w3-quarter w3-border-black')
+                ])
+        ] 
 )
 
 # Querying and Graphing Selected Data
 @app.callback(
     Output('interactive-graphic', 'figure'),
+    Output('dataTable', 'data'),
+    Output('dataTable', 'columns'),
     Input('district_or_circuit', 'value'),
     Input('race', 'value'),
     Input('gender', 'value'),
@@ -147,20 +188,30 @@ layout = html.Div(children = [
     Input('disposition_type', 'value'))
 def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, charge_type, dispo_code):
 
+    if district_or_circuit == None:
+        district_or_circuit = 'circuit'
+
+    if per_capita == None:
+        per_capita = 'True'
+
     if not bool(race_name):
         race_name = [*range(6)]
+
     if not bool(sex_name):
         sex_name = [*range(2)]
+
     if not bool(year):
         if district_or_circuit == 'circuit':
             year = [*range(min(full_data['circuit']), max(full_data['circuit']) + 1)]
         else:
             year = [*range(min(full_data['district']), max(full_data['district']) + 1)]
+    
     if not bool(charge_type):
         if district_or_circuit == 'circuit':
             charge_type = [*range(5)]
         else:
             charge_type = [*range(9)]
+    
     if not bool(dispo_code):
         if district_or_circuit == 'circuit':
             dispo_code = [*range(12)]
@@ -182,17 +233,10 @@ def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, cha
     transformed_data = pd.concat([transformed_data, empty_data])
     transformed_data = transformed_data.groupby(['FIPS', 'Race', 'Sex', 'ChargeType', 'DispositionCode']).sum().reset_index()
 
-    # Form query
-    race_code_str = '[' + ', '.join([str(x) for x in race_name]) + ']'
-    sex_code_str = '[' + ', '.join([str(x) for x in sex_name]) + ']'
-    charge_type_str = '[' + ', '.join([str(x) for x in charge_type]) + ']'
-    dispo_code_str = '[' + ', '.join([str(x) for x in dispo_code]) + ']'
-
-
-    query_str = '(' + ') & ('.join([f'Race in %s' % race_code_str,
-                        f'Sex == %s' % sex_code_str,
-                        f'ChargeType == %s' % charge_type_str,
-                        f'DispositionCode == %s' % dispo_code_str]) + ')'
+    query_str = '(' + ') & ('.join([f'Race in %s' % race_name,
+                        f'Sex == %s' % sex_name,
+                        f'ChargeType == %s' % charge_type,
+                        f'DispositionCode == %s' % dispo_code]) + ')'
 
     if len(year) < 2:
         transformed_data = transformed_data.query(query_str).groupby(['FIPS'])['count'].sum().reset_index()
@@ -202,11 +246,12 @@ def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, cha
     # Final Data in Dictionary, use Year to Index data
     if per_capita == 'True':
 
-        census_query_str = '(' + ') & ('.join([f'Race in %s' % race_code_str,
+        census_query_str = '(' + ') & ('.join([f'Race in %s' % race_name,
                                                f'Sex == %s' % sex_name, 
                                                f'YEAR == %s' % year]) + ')'
         
         census = census_data.query(census_query_str)
+        
         if len(year) < 2:
             census = census.groupby(['FIPS'])['population'].sum().reset_index()
         else:
@@ -227,6 +272,9 @@ def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, cha
             hover_name = 'Area',
             hover_data = ['population', 'count']
         )
+        
+        data = transformed_data[['Area', 'Per Capita Arrests']].sort_values(['Per Capita Arrests'], ascending=False).to_dict('records')
+        columns = [{"name": i, "id": i} for i in ['Area', 'Per Capita Arrests']]
     else:
         #  Auto Adjust Legend Range to Fit Value
         transformed_data['Area'] = transformed_data.FIPS.map(fips_map)
@@ -239,10 +287,13 @@ def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, cha
             labels={'count': 'arrests'},
             hover_name = 'Area'
         )
+        data = transformed_data[['Area', 'count']].sort_values(['count'], ascending=False).to_dict('records')
+        columns = [{"name": i, "id": i} for i in transformed_data[['Area', 'count']].columns]
+
     fig.update_geos(fitbounds="locations")
     fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
 
-    return fig
+    return fig, data, columns
 
 # Modifying Slider and Drop Down to be Dynamic
 @app.callback(
@@ -253,6 +304,9 @@ def update_graph(district_or_circuit, race_name, sex_name, year, per_capita, cha
     Input('district_or_circuit', 'value')
     )
 def update_dynamic_dropdowns(district_or_circuit):
+
+    if district_or_circuit == None:
+        district_or_circuit = 'circuit'
 
     charge_options = [{'label': val, 'value': key} for key, val in sorted(charge_map[district_or_circuit].items(), key=lambda item: item[1])]
     disposition_options = [{'label': val, 'value': key} for key, val in sorted(dispo_map[district_or_circuit].items(), key=lambda item: item[1])]
